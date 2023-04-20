53565b7f569c2b9fbbcf6c741a9a464d9831ddde
Vanya23 <ivpetrov87@yandex.ru>
2023-04-20 09:46:51 +0300

    step 1 v11

--- stats/stats-server/src/main/java/ru/practicum/ewm/server/stats/HitViewStatsReverseComparator.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/server/stats/HitViewStatsReverseComparator.java
@@ -4,10 +4,10 @@ import ru.practicum.ewm.dto.stats.ViewStats;
 
 import java.util.Comparator;
 
-public class HitViewStatsReverseComparator implements Comparator<ViewStats> {// сортировка по лайкам в обратном порядке
+public class HitViewStatsReverseComparator implements Comparator<ViewStats> {
 
     @Override
-    public int compare(ViewStats o1, ViewStats o2) {
+    public int compare(ViewStats o1, ViewStats o2){
         return -1 * o1.getHits().compareTo((o2.getHits()));
     }
 }
