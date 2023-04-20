0e830c02bf5a168c9b7b560fee1a255b336c6651
Vanya23 <ivpetrov87@yandex.ru>
2023-04-20 09:49:09 +0300

    step 1 v13

--- stats/stats-server/src/main/java/ru/practicum/ewm/server/stats/HitViewStatsReverseComparator.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/server/stats/HitViewStatsReverseComparator.java
@@ -7,7 +7,7 @@ import java.util.Comparator;
 public class HitViewStatsReverseComparator implements Comparator<ViewStats> {
 
     @Override
-    public int compare(ViewStats o1, ViewStats o2){
+    public int compare(ViewStats o1, ViewStats o2) {
         return -1 * o1.getHits().compareTo((o2.getHits()));
     }
 }
